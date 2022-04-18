fe72e87276ae303ab2a89c0855ff89d6c6ce9f26
Trixy20 <ngwepaola@gmail.com>
2022-04-18 22:00:27 +0200

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+"""A really awesome package.
+
+...
+"""
---
+++ nnedit/__init__.py
@@ -0,0 +1,7 @@
+""" This module is the init module of NNedit """
+__version__ = "0.1.0"
+
+
+def search(name, count=5):
+    """Search podcast by name."""
+    raise NotImplementedError()
